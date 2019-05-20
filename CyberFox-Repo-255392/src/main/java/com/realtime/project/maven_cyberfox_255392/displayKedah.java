e915cd4b4d9a30eafff71e5522ea4fcdc30276ab
prevenkumar <43874246+prevenkumar@users.noreply.github.com>
2019-05-21 00:41:56 +0800

    third commit
    updated

---
+++ CyberFox-Repo-255392/src/main/java/com/realtime/project/maven_cyberfox_255392/displayKedah.java
@@ -0,0 +1,66 @@
+package com.realtime.project.maven_cyberfox_255392;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.collections4.CollectionUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.*;
+
+class displayKedah {
+    private List<String> ValidURLList;
+    private Properties prop = new Properties();
+
+    displayKedah(List<String> ValidURLList) {
+        this.ValidURLList = ValidURLList;
+    }
+
+    void RetrievePlayer() throws IOException, ParseException {
+
+        System.out.println("\nDisplay All players From Kedah ......");
+        System.out.println("");
+        System.out.println("RK\t   |Sno\t |Name\t\t\t\t    |Rtg\t  |State\t  |Pts\t|Category");
+        System.out.println("---------------------------------------------------------------------------------------------");
+        for (String s : ValidURLList) {
+            Document doc = Jsoup.connect(s).get();
+        
+            Elements b = doc.select("div:nth-child(2)");
+            for (Element header : b) {
+                Elements h2 = header.select("h2");
+                String category = h2.text();
+                if (StringUtils.contains(category, "NATIONAL")) {
+                    String showCat = StringUtils.substringAfter(category, "9");
+                    Elements a = doc.select("table.CRs1 tr");
+                    for (Element row : a) {
+                        String state = row.select("td:nth-child(7)").text();
+                        String rank = row.select("td:nth-child(1)").text();
+                        String sno = row.select("td:nth-child(2)").text();
+                        String name = row.select(" td:nth-child(4)").text();
+                        String rtg = row.select("td.CRr").text();
+                        String pts = row.select("td:nth-child(8)").text();
+
+                        if (state.contains("KEDAH")) {
+                            ShowPlayer(rank, sno, name, rtg, pts, showCat);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void ShowPlayer(String rank, String sNo, String name, String rtg, String  replace, String showCat) {
+        try (InputStream input = new FileInputStream("config.properties")) {    // load a properties file
+            prop.load(input);
+            System.out.printf("\n%-5s  |  %-5s  |  %-30s | %-8s | %-8s  | %-8s | %-8s", rank, sNo, name, rtg, prop.getProperty("state"), replace, showCat);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
